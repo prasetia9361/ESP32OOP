fb83b9d2f597e44a2164a0bc87bf1fac07210304
prasetia9361 <prasetia1805@gmail.com>
2024-03-20 00:01:51 +0700

    first commit

--- src/eeprom/eeprom.h
+++ src/eeprom/eeprom.h
@@ -2,14 +2,20 @@
 
 class eeprom {
     private:
-    String value;
-    int addres;
-    int length;
+     String valueSsid;
+     String valuePass;
     public:
      eeprom();
      void setup(int size);
-     void eepromWifi(int address, int max);
-     void readeeprom(int addres, int length);
-     void writewifi(String xas, String yas);
-     String wifiValue() const;
+    //  void eepromWifi(int address, int max);
+     String eepromSsid(int addressSsid,int lengthSsid);
+     String eepromPass(int addressPass,int lengthPass);
+    //  void readSSid(int addressSsid, int lengthSsid);
+    //  void readPass(int addressPass, int lengthPass);
+    //  void writewifi(String xas, String yas);
+    //  String wifiValue() const;
+     void handleRoot();
+     void handleFrom();
+     void settingWifi();
+     void notConnect();
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
 #include <Arduino.h>
-#include <WebServer.h>
+// #include <WebServer.h>
 #include "./eeprom/eeprom.h"
 #include "./comunication/comunication.h"
-#include "HTML.h"
+// #include "HTML.h"
 eeprom prom;
 comunication com;
 int valor;
-WebServer server(80);
+// WebServer server(80);
 
-String ssid = "Setting_WiFi";
-String pass = "1qazxsw2";
-String ssidNew = "";
-String passNew;
-String ssidwifi;
-String passwifi;
+#define ssid "Setting_WiFi"
+#define pass "1qazxsw2"
+// String ssidNew = "";
+// String passNew;
+String ssidWifi;
+String passWifi;
 
 uint64_t chipId;
 
@@ -22,49 +22,126 @@ long lastSendTime = 0;     // last send time
 int interval = 2000;       // interval between sends
 int length = 512;
 
-void handleRoot() {
-  server.send(200, "text/html", index_html);
-}
-void handleForm() {
-  ssidNew = server.arg("ssidNew");
-  passNew = server.arg("passNew");
-  server.send(200, "text/html", sukses_html);
-  delay(2000);
-  prom.writewifi(ssidNew, passNew);
-  Serial.println("setting wifi berhasil");
-  ESP.restart();
-}
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+TaskHandle_t Task3;
+TaskHandle_t Task4;
 
+void wifiBegin(void* pvParameters);
+void LoraBegin(void* pvParameters);
+void mqttBegin(void* pvParameters);
+void sending(void* pvParameters);
 void setup() {
   Serial.begin(115200);
   Serial.println("LoRa Receiver");
   prom.setup(length);
-  prom.readeeprom(0,32);
-  ssidwifi = prom.wifiValue();
-  prom.readeeprom(32,96);
-  passwifi = prom.wifiValue();
+  xTaskCreatePinnedToCore(
+    wifiBegin,
+    "Task1",
+    1000,
+    NULL,
+    10,
+    &Task1,
+    0
+  );
+
+  // prom.setup(length);
+  // ssidWifi = prom.eepromSsid(0, 20);
+  // passWifi = prom.eepromPass(20, 50);
+  // // client.begin("broker.emqx.io", net);
+  // chipId = ESP.getEfuseMac();
+  // Serial.println(ssidWifi);
+  // Serial.println(passWifi);
+  // com.wifiConnect(ssidWifi, passWifi);
+  xTaskCreatePinnedToCore(
+    LoraBegin,
+    "Task2",
+    1000,
+    NULL,
+    10,
+    &Task2,
+    1
+  );
+  // com.loraconnect();
+  // Serial.println("LoRa Active");
+  xTaskCreatePinnedToCore(
+    wifiBegin,
+    "Task3",
+    1000,
+    NULL,
+    8,
+    &Task3,
+    0
+  );
+  // if (!com.wifiStatus()) {
+  //   com.wifiAPconnect(ssid,pass);
+  //   prom.settingWifi();
+  //   // server.on("/", handleRoot);  // Routine untuk menghandle homepage
+  //   // server.on("/action_page", handleForm);
+  //   // server.begin();
+  // } else if (com.wifiStatus()) {
+  //   com.wifiAP();
+  //   com.mqttconnect(String(chipId) + "/#");
+  // }
+}
+
+void loop() {
+  xTaskCreatePinnedToCore(
+    sending,
+    "Task4",
+    1000,
+    NULL,
+    8,
+    &Task4,
+    1
+  );
+  // if (!com.wifiStatus()) {
+  //   prom.notConnect();
+  //   // server.handleClient();
+  // } else {
+  //   // timer.run();
+  //   com.mqttloop();
+  //   if (com.mqttlost()) {
+  //       com.wifiAP();
+  //       com.mqttconnect(String(chipId) + "/#");
+  //   }
+  //   com.onRecive(chipId);
+  //   // onRecive(LoRa.parsePacket());
+  // }
+}
+
+void wifiBegin(void* pvParameters){
+  ssidWifi = prom.eepromSsid(0, 20);
+  passWifi = prom.eepromPass(20, 50);
   // client.begin("broker.emqx.io", net);
   chipId = ESP.getEfuseMac();
+  Serial.println(ssidWifi);
+  Serial.println(passWifi);
+  com.wifiConnect(ssidWifi, passWifi);
+}
+
+void LoraBegin(void* pvParameters){
   com.loraconnect();
-  Serial.println(ssidwifi);
-  Serial.println(passwifi);
-  com.wifiConnect(ssidwifi, passwifi);
   Serial.println("LoRa Active");
-  if (!com.wifiStatus()) {
+}
+
+void mqttBegin(void* pvParameters){
+   if (!com.wifiStatus()) {
     com.wifiAPconnect(ssid,pass);
-    server.on("/", handleRoot);  // Routine untuk menghandle homepage
-    server.on("/action_page", handleForm);
-    server.begin();
+    prom.settingWifi();
+    // server.on("/", handleRoot);  // Routine untuk menghandle homepage
+    // server.on("/action_page", handleForm);
+    // server.begin();
   } else if (com.wifiStatus()) {
     com.wifiAP();
     com.mqttconnect(String(chipId) + "/#");
   }
 }
 
-void loop() {
-
+void sending(void* pvParameters){
   if (!com.wifiStatus()) {
-    server.handleClient();
+    prom.notConnect();
+    // server.handleClient();
   } else {
     // timer.run();
     com.mqttloop();
