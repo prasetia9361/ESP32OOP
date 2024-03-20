eff106d4ba5fee83d4d148b73c9311c5a7ebb725
mazwas <waskito.djoyobisonoo@gmail.com>
2024-03-20 21:49:01 +0700

    Multicore Fixing

--- src/comunication/comunication.cpp
+++ src/comunication/comunication.cpp
@@ -7,65 +7,77 @@
 WiFiClient net;
 MQTTClient client;
 
-comunication::comunication(){}
+comunication::comunication() {}
 
-void comunication::wifiAP(){
-    state = true;
-    WiFi.softAPdisconnect(state);
-    WiFi.mode(WIFI_STA);
-    Serial.println("menghubungkan ke mqtt");
-    Serial.print("connecting");
+void comunication::wifiAP()
+{
+  state = true;
+  WiFi.softAPdisconnect(state);
+  WiFi.mode(WIFI_STA);
+  Serial.println("menghubungkan ke mqtt");
+  Serial.print("connecting");
 }
 
-void comunication::wifiConnect(String ssid, String pass){
-    WiFi.begin(ssid, pass);
+void comunication::wifiConnect(String ssid, String pass)
+{
+  WiFi.begin(ssid, pass);
 }
 
-bool comunication::wifiStatus(){
-    return WiFi.status() == WL_CONNECTED;
+bool comunication::wifiStatus()
+{
+  return WiFi.status() == WL_CONNECTED;
 }
 
-void comunication::wifiAPconnect(String ssid, String pass){
-    WiFi.softAP(ssid, pass);
-    IPAddress IP = WiFi.softAPIP();
-    Serial.println("");
-    Serial.print("IP Address: ");
-    Serial.println(IP);
+void comunication::wifiAPconnect(String ssid, String pass)
+{
+  WiFi.softAP(ssid, pass);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.println("");
+  Serial.print("IP Address: ");
+  Serial.println(IP);
 }
 
-void comunication::mqttloop(){
-    client.loop();
+void comunication::mqttloop()
+{
+  client.loop();
 }
 
-bool comunication::mqttlost(){
-    return !client.connected();
+bool comunication::mqttlost()
+{
+  return !client.connected();
 }
 
-void comunication::mqttconnect(String text){
-    client.begin("broker.emqx.io", net);
-    while (!client.connect("ESP32")) {
-        Serial.print(".");
-        delay(1000);
-    }
-    Serial.print("mqtt connected");
-    client.subscribe(text);//String(chipId) + "/#"
+void comunication::mqttconnect(String text)
+{
+  client.begin("broker.emqx.io", net);
+  while (!client.connect("ESP32"))
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.print("mqtt connected");
+  client.subscribe(text); // String(chipId) + "/#"
 }
 
-void comunication::loraconnect(){
-    LoRa.setPins(ss, rst, dio0);
-    if (!LoRa.begin(433E6)) {
-        Serial.println("Starting LoRa failed!");
-        while (1)
-        ;
-    }
+void comunication::loraconnect()
+{
+  LoRa.setPins(ss, rst, dio0);
+
+  while (!LoRa.begin(433E6))
+  {
+    Serial.println("Starting LoRa failed!");
+  }
 }
 
-String comunication::getValue(String data, char separator, int index) {
+String comunication::getValue(String data, char separator, int index)
+{
   int found = 0;
-  int strIndex[] = { 0, -1 };
+  int strIndex[] = {0, -1};
   int maxIndex = data.length() - 1;
-  for (int i = 0; i <= maxIndex && found <= index; i++) {
-    if (data.charAt(i) == separator || i == maxIndex) {
+  for (int i = 0; i <= maxIndex && found <= index; i++)
+  {
+    if (data.charAt(i) == separator || i == maxIndex)
+    {
       found++;
       strIndex[0] = strIndex[1] + 1;
       strIndex[1] = (i == maxIndex) ? i + 1 : i;
@@ -74,29 +86,33 @@ String comunication::getValue(String data, char separator, int index) {
   return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
-void comunication::sendtemp(uint64_t Id) {
-  client.publish(String(Id) + "/suhu", temp);//
+void comunication::sendtemp(uint64_t Id)
+{
+  client.publish(String(Id) + "/suhu", temp); //
   Serial.println("data suhu terkirim lewat" + String(Id) + "/suhu");
   Serial.println(temp);
   // 106151743883988/suhu
   // delay(100);
 }
 
-void comunication::sendlong(uint64_t Id) {
+void comunication::sendlong(uint64_t Id)
+{
   client.publish(String(Id) + "/long", longitude);
   Serial.println("data longitude terkirim lewat" + String(Id) + "/long");
   Serial.println(longitude);
   // 106151743883988/long
   // delay(100);
 }
-void comunication::sendlat(uint64_t Id) {
+void comunication::sendlat(uint64_t Id)
+{
   client.publish(String(Id) + "/lat", latitude);
   Serial.println("data latitude terkirim lewat" + String(Id) + "/lat");
   Serial.println(latitude);
   // 106151743883988/lat
   // delay(100);
 }
-void comunication::sendspeed(uint64_t Id) {
+void comunication::sendspeed(uint64_t Id)
+{
   client.publish(String(Id) + "/speed", speed);
   Serial.println("data speed terkirim lewat" + String(Id) + "/speed");
   Serial.println(speed);
@@ -104,7 +120,8 @@ void comunication::sendspeed(uint64_t Id) {
   // delay(100);
 }
 
-void comunication::sendVolatge(uint64_t Id) {
+void comunication::sendVolatge(uint64_t Id)
+{
   client.publish(String(Id) + "/vol", voltage);
   Serial.println("data speed terkirim lewat" + String(Id) + "/vol");
   Serial.println(voltage);
@@ -112,7 +129,8 @@ void comunication::sendVolatge(uint64_t Id) {
   // delay(100);
 }
 
-void comunication::sendCurrent(uint64_t Id) {
+void comunication::sendCurrent(uint64_t Id)
+{
   client.publish(String(Id) + "/current", current);
   Serial.println("data speed terkirim lewat" + String(Id) + "/current");
   Serial.println(current);
@@ -120,52 +138,59 @@ void comunication::sendCurrent(uint64_t Id) {
   // delay(100);
 }
 
-void comunication::onRecive(uint64_t numId){
-    packetSize =  LoRa.parsePacket();
-    if (packetSize == 0) return;
-    memset(cadena, 0, sizeof(cadena));  //memset deletes the contents of the array "cadena" from position 0 to the end sizeof
-    // read packet header bytes:
-    int recipient = LoRa.read();       // recipient address
-    byte sender = LoRa.read();         // sender address
-    byte incomingMsgId = LoRa.read();  // incoming msg ID
-    // byte incomingLength = LoRa.read();  // incoming msg length
-    // read packet
-    // received a packet
-    // Serial.print("Received packet... ");
-    while (LoRa.available()) {
-        char dedos = (char)LoRa.read();
-        Serial.print(dedos);
-        cadena[posicion] = dedos;  //Read a character from the string "cadena" from "posicion", then read the next character with "posicion++"
-        posicion++;
-    }
-    posicion = 0;
-    String mkrwan = String(sender, HEX);
-    String esp32 = String(recipient, HEX);
-    Serial.println("Received from: 0x" + mkrwan);
-    Serial.println("Sent to: 0x" + esp32);
-    Serial.println("Message ID: " + String(incomingMsgId));
-    Serial.println("RSSI: " + String(LoRa.packetRssi()));
-    Serial.println("Snr: " + String(LoRa.packetSnr()));
+void comunication::onRecive(uint64_t numId)
+{
+  packetSize = LoRa.parsePacket();
+  if (packetSize == 0)
+    return;
+  memset(cadena, 0, sizeof(cadena)); // memset deletes the contents of the array "cadena" from position 0 to the end sizeof
+  // read packet header bytes:
+  int recipient = LoRa.read();      // recipient address
+  byte sender = LoRa.read();        // sender address
+  byte incomingMsgId = LoRa.read(); // incoming msg ID
+  // byte incomingLength = LoRa.read();  // incoming msg length
+  // read packet
+  // received a packet
+  // Serial.print("Received packet... ");
+  while (LoRa.available())
+  {
+    char dedos = (char)LoRa.read();
+    Serial.print(dedos);
+    cadena[posicion] = dedos; // Read a character from the string "cadena" from "posicion", then read the next character with "posicion++"
+    posicion++;
+  }
+  posicion = 0;
+  String mkrwan = String(sender, HEX);
+  String esp32 = String(recipient, HEX);
+  Serial.println("Received from: 0x" + mkrwan);
+  Serial.println("Sent to: 0x" + esp32);
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
 
-    temp = getValue(cadena, ',', 1);
-    latitude = getValue(cadena, ',', 2);
-    longitude = getValue(cadena, ',', 3);
-    speed = getValue(cadena, ',', 4);
-    voltage = getValue(cadena,',',5);
-    current = getValue(cadena,',',6);
+  temp = getValue(cadena, ',', 1);
+  latitude = getValue(cadena, ',', 2);
+  longitude = getValue(cadena, ',', 3);
+  speed = getValue(cadena, ',', 4);
+  voltage = getValue(cadena, ',', 5);
+  current = getValue(cadena, ',', 6);
 
-    if (recipient == localAddress && sender == destination) {
-        if (millis() - sending >= 1000) {
-        sending = millis();
-        sendtemp(numId);
-        sendlong(numId);
-        sendlat(numId);
-        sendspeed(numId);
-        sendVolatge(numId);
-        sendCurrent(numId);
-        }
-    } else {
-        Serial.println("tidak ada data");
-        return;
+  if (recipient == localAddress && sender == destination)
+  {
+    if (millis() - sending >= 1000)
+    {
+      sending = millis();
+      sendtemp(numId);
+      sendlong(numId);
+      sendlat(numId);
+      sendspeed(numId);
+      sendVolatge(numId);
+      sendCurrent(numId);
     }
+  }
+  else
+  {
+    Serial.println("tidak ada data");
+    return;
+  }
 }
